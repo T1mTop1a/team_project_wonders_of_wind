891a8371ce113b656057e0b3fcd1d93b519c0563
Wind Wong <2612916W@student.gla.ac.uk>
2022-11-03 06:32:24 +0000

    implement basic page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component }  from 'react';
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
 function App() {
 
@@ -20,7 +18,6 @@ function App() {
     let json = await response.json();
     console.log(json);
   }
-  // importing material UI components
   
   function Header() {
     return (
@@ -37,10 +34,81 @@ function App() {
     );
   }
 
+
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+  
+  const LineChart = () => {
+    return (
+      <div>
+        <Line data={data} width={"80%"} options={{ maintainAspectRatio: false }}/>
+      </div>
+    );
+  };
+
+  const InputBox = (props) => {
+    return (
+      <div>
+        <input class="inputBox" style={{
+          width: '500px',
+          height: '50px',
+          marginLeft: '64px',
+          marginTop: '3px',
+          marginBottom: '3px',
+          textIndent: '10px',
+        }} placeholder={props.text}></input>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <Header/>
-      <button onClick={getExampleData}>Get Example Data</button>
+      <form style={{
+        marginTop: '56px',
+      }}>
+        <InputBox text="input your turbine location"/>
+        <InputBox text="input your turbine model"/>
+        <div style={{
+          width: '500px',
+          height: '50px',
+          marginLeft: '70px',
+          marginTop: '9px',
+          marginBottom: '14px',
+          alignContent: 'right',
+          display: 'flex'
+        }}>
+          <button style={{
+            background: '#193C0D',
+            width: '128px',
+            height: '51px',
+            left: '433px',
+            top: '295px',
+            border: '0',
+            fontFamily: 'Roboto',
+            fontWeight: '600',
+            fontSize: '20px',
+            color: '#FFFFFF',
+            marginRight: '0',
+            marginLeft: 'auto',
+          }}>Search</button>
+        </div>
+      </form>
+      <div id="chartContainer" style={{
+      }}>
+        <LineChart/>
+      </div>
     </div>
   );
 }
