21640e57301a69a1332c0254850afdd3fafe97c3
Sofia Karamichali <2570646k@student.gla.ac.uk>
2022-11-30 11:24:51 +0000

    Signup and Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
-import React from 'react';
-//import React, { useState, useEffect }  from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 
-import Header from './pages/home';
-import Home from './pages/homepage';
+import Header from "./pages/home";
+import Home from "./pages/homepage";
+import SignUp from "./pages/signup";
+import LogIn from "./pages/login";
 
 //var moment = require('moment-timezone');
 
 function App() {
- 
-  return(
-  <Router>
-    <Routes>
-      <Route exact2 path='/' exact element={<Home />} />
-      <Route exact1 path='/header' exact element={<Header />} />
-      
-
-    </Routes>
-  </Router>
-  
-  )
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" exact element={<Home />} />
+        <Route exact path="/header" exact element={<Header />} />
+        <Route exact path="/signup" exact element={<SignUp />} />
+        <Route exact path="/login" exact element={<LogIn />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
 //import Home from './homepage.js';
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 //import './App.css';
 //import React, { useState, useEffect }  from 'react';
@@ -11,35 +10,35 @@ import Toolbar from "@mui/material/Toolbar";
 //import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
-
 //var moment = require('moment-timezone');
 
 const Header = () => {
+  let appName = "Wonders of Wind";
 
-  let appName = "Wonders of Wind"
-
-    return (
-        <>
-        
-        <AppBar class="NavBar" >
-          <Toolbar>
-              <Link style={{textDecoration: 'none'}} to="/" className="link">
-                    <Button variant="h6" align="left"
-                    component="div" sx={{ mr: 2, flexGrow: 1 }}>
-                    {appName}
-                    </Button>
-            </Link>
-            <Button   class="linkBoxes">Login </Button>
-            <Button  class="linkBoxes">Sign up</Button>
-            
-          </Toolbar>
-        </AppBar>
-
-
-        </>
-    );
-  }
-
-
+  return (
+    <>
+      <AppBar class="NavBar">
+        <Toolbar>
+          <Link style={{ textDecoration: "none" }} to="/" className="link">
+            <Button
+              variant="h6"
+              align="left"
+              component="div"
+              sx={{ mr: 2, flexGrow: 1 }}
+            >
+              {appName}
+            </Button>
+          </Link>
+          <Link to="/login">
+            <Button class="linkBoxes">Login </Button>
+          </Link>
+          <Link to="/signup">
+            <Button class="linkBoxes">Sign up</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default Header;
